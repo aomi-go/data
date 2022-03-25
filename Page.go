3736883279d9715b0b3e01490721b3d88379dc5b
尼古拉斯丶少 <sean.snow@live.com>
2022-03-25 17:41:29 +0800

    feat: 创建 page 方法

--- Page.go
+++ Page.go
@@ -1,12 +1,40 @@
 package data_common
 
-type Page[T interface{}] struct {
-	Number        uint `json:"number,omitempty"`
-	Size          uint `json:"size,omitempty"`
-	Content       []T  `json:"content,omitempty"`
-	First         bool `json:"first,omitempty"`
-	Last          bool `json:"last,omitempty"`
-	TotalPages    uint `json:"totalPages,omitempty"`
-	TotalElements uint `json:"totalElements,omitempty"`
-	Value         any  `json:"value,omitempty"`
+import "math"
+
+type Page struct {
+	Number        int64          `json:"number,omitempty"`
+	Size          int64          `json:"size,omitempty"`
+	Content       *[]interface{} `json:"content,omitempty"`
+	First         bool           `json:"first,omitempty"`
+	Last          bool           `json:"last,omitempty"`
+	TotalPages    int64          `json:"totalPages,omitempty"`
+	TotalElements int64          `json:"totalElements,omitempty"`
+	Value         any            `json:"value,omitempty"`
+}
+
+func Of(data *[]interface{}, p Pageable, totals int64) *Page {
+
+	var pageable Pageable
+	if nil == p {
+		pageable = PageRequest{Page: 0, Size: 0}
+	}
+
+	var totalPages int64
+	if pageable.GetPageSize() == 0 {
+		totalPages = 1
+	} else {
+		tmp := totals / pageable.GetPageSize()
+		totalPages = int64(math.Ceil(float64(tmp)))
+	}
+
+	return &Page{
+		Number:        pageable.GetPageNumber(),
+		Size:          pageable.GetPageSize(),
+		Content:       data,
+		First:         pageable.GetPageNumber() == 0,
+		Last:          pageable.GetPageNumber()+1 == totalPages,
+		TotalPages:    totalPages,
+		TotalElements: totals,
+	}
 }
