981fc2dc0af28f3a517fd185b932ef6128d298ba
xxoo <make@xx.oo>
2024-06-19 21:08:37 +0800

    fix: rename id field

--- common/entity/mongo/entity.go
+++ common/entity/mongo/entity.go
@@ -3,18 +3,18 @@ package mongo
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AbstractEntity struct {
-	ID string `bson:"_id,omitempty"`
+	Id string `bson:"_id,omitempty"`
 }
 
 func (a *AbstractEntity) GetId() interface{} {
-	return a.ID
+	return a.Id
 }
 
 func (a *AbstractEntity) SetId(id interface{}) {
 	switch i := id.(type) {
 	case primitive.ObjectID:
-		a.ID = i.Hex()
+		a.Id = i.Hex()
 	case string:
-		a.ID = i
+		a.Id = i
 	}
 }
