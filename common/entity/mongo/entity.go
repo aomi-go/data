ffeb212b898b05074b41bc86024a7b8e5e018665
xxoo <make@xx.oo>
2024-06-30 14:17:34 +0800

    feat: 字段类型

--- common/entity/mongo/entity.go
+++ common/entity/mongo/entity.go
@@ -3,7 +3,7 @@ package mongo
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AbstractEntity struct {
-	Id string `bson:"_id,omitempty"`
+	Id primitive.ObjectID `bson:"_id,omitempty"`
 }
 
 func (a *AbstractEntity) GetId() interface{} {
@@ -13,8 +13,11 @@ func (a *AbstractEntity) GetId() interface{} {
 func (a *AbstractEntity) SetId(id interface{}) {
 	switch i := id.(type) {
 	case primitive.ObjectID:
-		a.Id = i.Hex()
-	case string:
 		a.Id = i
+	case string:
+		oid, err := primitive.ObjectIDFromHex(i)
+		if err == nil {
+			a.Id = oid
+		}
 	}
 }
