5422c18c9aa10c7a8ea3edfc41f9b99f5bf567a7
xxoo <make@xx.oo>
2024-06-30 14:20:37 +0800

    feat: 使用指针

--- common/entity/mongo/entity.go
+++ common/entity/mongo/entity.go
@@ -3,7 +3,7 @@ package mongo
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AbstractEntity struct {
-	Id primitive.ObjectID `bson:"_id,omitempty"`
+	Id *primitive.ObjectID `bson:"_id,omitempty"`
 }
 
 func (a *AbstractEntity) GetId() interface{} {
@@ -13,11 +13,11 @@ func (a *AbstractEntity) GetId() interface{} {
 func (a *AbstractEntity) SetId(id interface{}) {
 	switch i := id.(type) {
 	case primitive.ObjectID:
-		a.Id = i
+		a.Id = &i
 	case string:
 		oid, err := primitive.ObjectIDFromHex(i)
 		if err == nil {
-			a.Id = oid
+			a.Id = &oid
 		}
 	}
 }
